ffde9148e8e548606d451b333dd191cb37d191ed
midnight-miner <midnight.miner.crypto@gmail.com>
2016-09-19 03:02:41 -0400

    Fix comments and typos.

--- src/base58.h
+++ src/base58.h
@@ -272,7 +272,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 25,
+        PUBKEY_ADDRESS = 25,  // BATA addresses start with B
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
