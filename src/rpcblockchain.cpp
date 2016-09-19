734fbe5a17c38fa1d41689e4d95950a8c0c43ee0
midnight-miner <midnight.miner.crypto@gmail.com>
2016-09-19 03:02:41 -0400

    Fix comments and typos.

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -106,8 +106,8 @@ Value settxfee(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() < 1 || params.size() > 1)
         throw runtime_error(
-            "settxfee <amount BATA/KB>\n"
-            "<amount> is a real and is rounded to the nearest 0.00000001 BATA per KB");
+            "settxfee <amount BTA/KB>\n"
+            "<amount> is a real and is rounded to the nearest 0.00000001 BTA per KB");
 
     // Amount
     int64 nAmount = 0;
