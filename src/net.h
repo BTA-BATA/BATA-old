87dce157d1b1ece63e0456e982a556b2d61d6b71
JustinPercy <justin@biznaturally.ca>
2017-09-07 11:49:19 -0500

    Refresh=4hours

--- src/net.h
+++ src/net.h
@@ -47,7 +47,7 @@ static const int IDLE_TIMEOUT = 4 * 60;
 /** Time between cycles to check for idle nodes, force disconnect (seconds) **/ 
 static const int DATA_TIMEOUT = 3 * 60;
 /** Time between cycles force reconnection of disconnections from peers.dat (minutes) **/ 
-static const int REFRESH_CONNECTIONS = 25;
+static const int REFRESH_CONNECTIONS = 240;
 /** The maximum number of entries in an 'inv' protocol message */
 static const unsigned int MAX_INV_SZ = 50000;
 /** The maximum number of new addresses to accumulate before announcing. */
